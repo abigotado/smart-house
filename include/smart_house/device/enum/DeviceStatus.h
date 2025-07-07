315628806d159f74cbe8c104eaafe4dd980993b3
Nikita Kovalenko <nik.koval.89@gmail.com>
2025-07-07 19:50:34 -0300

    feature(app): сreate cmake structure and enums

---
+++ include/smart_house/device/enum/DeviceStatus.h
@@ -0,0 +1,15 @@
+#pragma once
+
+/**
+ * @brief Статус устройства
+ */
+enum class DeviceStatus {
+    /**
+     * @brief В сети
+     */
+    ONLINE,
+    /**
+     * @brief Не в сети
+     */
+    OFFLINE
+};
---
+++ include/smart_house/room/enum/RoomType.h
@@ -0,0 +1,31 @@
+#pragma once
+
+/**
+ * @brief Тип комнаты
+ */
+enum class RoomType {
+    /**
+     * @brief Гостиная
+     */
+    LIVING_ROOM,
+    /**
+     * @brief Спальня
+     */
+    BEDROOM,
+    /**
+     * @brief Коридор
+     */
+    CORRIDOR,
+    /**
+     * @brief Ванная комната
+     */
+    RESTROOM,
+    /**
+     * @brief Кухня
+     */
+    KITCHEN,
+    /**
+     * @brief Балкон
+     */
+    BALCONY
+};
